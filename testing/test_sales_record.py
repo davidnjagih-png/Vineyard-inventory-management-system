a34c230fcb1b8356a581189675a1d51a3b1f4021
davidnjagih-png <david.njagih@student.moringaschool.com>
2026-09-08 14:08:58 +0300

    test code made

---
+++ testing/test_sales_record.py
@@ -0,0 +1,10 @@
+import pytest
+from models.sales_record import SalesRecord
+
+def test_sales_record_creation():
+    record = SalesRecord(item="red wine", quantity=50, price=10)
+    assert record.item == "red wine"
+    assert record.quantity == 50
+    assert record.price == 10
+
+
