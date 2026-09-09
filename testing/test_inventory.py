25517481fb42e28afd96dba6e57cd66eff9d74a8
Harry Salim <salimharry96@gmail.com>
2026-09-09 19:32:58 +0400

    Added inventory test structure

---
+++ testing/test_inventory.py
@@ -0,0 +1,14 @@
+def test_add_wine_batch():
+    pass
+
+def test_wine_batch_has_correct_details():
+    pass
+
+def test_add_grape_stock():
+    pass
+
+def test_remove_grape_stock():
+    pass
+
+def test_cannot_remove_more_grapes_than_available():
+    pass
