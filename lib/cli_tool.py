6f7d8d07836c951e0c655c4e487a34ff22f86762
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-09-10 15:04:07 +0300

    seperate Cli auth parsing logic to different classes

--- lib/cli_tool.py
+++ lib/cli_tool.py
@@ -1,29 +1,8 @@
 import argparse
-import getpass
 
-from lib.auth import authenticated, delete_user_session, save_user_session
+from lib.auth import authenticated
 
-"""
-Todo: persist state in file when login confirmed and create decorator to authenticate other processes if user id logged in.
-"""
-
-
-def handle_login(args):
-    """Login user and store creds in file"""
-    print("Username: ", args.username)
-
-    if args.password is None:
-        password = getpass.getpass(prompt=f"Password for {args.username}:")
-        print("password: ", password)
-    # Todo: validate user exists in user json file before creating session
-    # login user by saving session
-    save_user_session(args.username)
-
-
-@authenticated
-def handle_logout(args):
-    """Delete user session on user logout"""
-    delete_user_session()
+from .auth_cli import AuthCli
 
 
 @authenticated
@@ -38,19 +17,20 @@ def main():
     )
     subparser = parser.add_subparsers()
 
+    # Authentication
+    auth_cli = AuthCli(subparser=subparser)
     # login
-    login_parser = subparser.add_parser(
-        "login", help="Login to application providing a username"
-    )
-    login_parser.add_argument("username", help="Enter the account username.")
-    login_parser.add_argument(
-        "password", nargs="?", default=None, help="Enter the account password"
-    )
-    login_parser.set_defaults(func=handle_login)
-
+    auth_cli.login()
     # logout
-    logout_parser = subparser.add_parser("logout", help="Logout of the application")
-    logout_parser.set_defaults(func=handle_logout)
+    auth_cli.logout()
+    # Wine batch management
+    """
+    todo: Add new wine (@params: type[red,white,rose], vintage, quantity) , Edit wine, Delete, View wines. (Sales 'r',Owner 'r', Admin 'r,w' ) 
+    """
+    # add wine
+    add_wine_parser = subparser.add_parser(
+        "wines", help="Add, Edit, Delete, View wines"
+    )
 
     # demo parser
     demo_parser = subparser.add_parser("demo", help="Testing authentication")
