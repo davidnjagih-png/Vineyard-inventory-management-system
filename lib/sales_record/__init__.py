27a27c17e9bf2fff46a1511bfa4003d948cfea41
davidnjagih-png <david.njagih@student.moringaschool.com>
2026-09-10 13:04:36 +0300

    Add Sales Record Module with Pricing, Projections, and Reporting (#11)
    * sale-record folder and files created
    
    * sales-record code made
    
    * init code made to make the folder a package
    
    * pricing code added
    
    * projections code added to calculate the projections
    
    * reporting code added to generate reports for the sales
    
    * sales folder renamed

---
+++ lib/sales_record/__init__.py
@@ -0,0 +1,1 @@
+from .sales_record import SalesRecord
--- lib/sales_record/sales_record.py
+++ lib/sales_record/sales_record.py
@@ -1,34 +1,24 @@
-import pytest
-from lib.sales_record.sales_record import SalesRecord
+from lib.sales_record.pricing import price_wine, price_grapes
+from lib.sales_record.projections import calculate_projection
 from lib.sales_record.reporting import generate_sales_report
 
-def test_view_wine_sales_projection():
-    record = SalesRecord(item="red wine", quantity=100, price_per_unit=750)
-    projection = record.calculate_projection()
-    assert projection == 100 * 750
+class SalesRecord:
+    def __init__(self, item, quantity, price_per_unit):
+        self.item = item
+        self.quantity = quantity
+        self.price_per_unit = price_per_unit
 
-def test_price_wine_by_type():
-    record = SalesRecord(item="rose wine", quantity=50, price_per_unit=600)
-    assert record.item == "rose wine"
-    assert record.price_per_unit == 600
-    assert record.calculate_projection() == 50 * 600
+    def calculate_projection(self):
+        return calculate_projection(self.quantity, self.price_per_unit)
 
-def test_view_table_grape_projection():
-    record = SalesRecord(item="table grapes", quantity=40, price_per_unit=100)
-    projection = record.calculate_projection()
-    assert projection == 40 * 100
+    def price_item(self):
+        if "wine" in self.item.lower():
+            return price_wine(self.item, self.quantity, self.price_per_unit)
+        elif "grape" in self.item.lower():
+            return price_grapes(self.quantity, self.price_per_unit)
+        else:
+            raise ValueError("Unknown item type")
 
-def test_price_table_grapes_by_packet():
-    record = SalesRecord(item="table grapes", quantity=10, price_per_unit=120)
-    assert record.item == "table grapes"
-    assert record.calculate_projection() == 10 * 120
-
-def test_generate_sales_report():
-    wine = SalesRecord(item="white wine", quantity=20, price_per_unit=700)
-    grapes = SalesRecord(item="table grapes", quantity=15, price_per_unit=90)
-
-    report = generate_sales_report([wine, grapes])
-
-    assert report["wine_sales"] == 20 * 700
-    assert report["grape_sales"] == 15 * 90
-    assert report["total_sales"] == (20 * 700) + (15 * 90)
+    @staticmethod
+    def report(records):
+        return generate_sales_report(records)
