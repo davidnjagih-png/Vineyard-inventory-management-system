e14fdd99a1ae51effea2182ef857b82074094705
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-09-13 08:57:07 +0300

    inventory db initialization

---
+++ lib/inventory/inventory_db.py
@@ -0,0 +1,38 @@
+import json
+import os
+
+from .grape_stock import GrapeStock
+from .wine_batch import WineBatch
+
+
+def add_inventory_item(item):
+    if os.path.exists("inventory_db.json") and os.path.getsize("inventory_db.json") > 0:
+        with open("inventory_db.json", "r") as file:
+            data = json.load(file)
+    else:
+        data = {"wine": [], "grapes": []}
+
+    if isinstance(item, WineBatch):
+        data["wine"].append(
+            {
+                "batch_id": item.batch_id,
+                "wine_type": item.wine_type,
+                "vintage": item.vintage,
+                "quantity": item.quantity,
+            }
+        )
+    elif isinstance(item, GrapeStock):
+        data["grapes"].append({})
+
+    with open("inventory_db.json", "w") as file:
+        json.dump(data, file, indent=4)
+        print("Inventory item added successfuly.")
+
+
+def get_inventory_items():
+    if os.path.exists("inventory_db.json") and os.path.getsize("inventory_db.json") > 0:
+        with open("inventory_db.json", "r") as file:
+            data = json.load(file)
+    else:
+        data = {"wine": [], "grapes": []}
+    return data
